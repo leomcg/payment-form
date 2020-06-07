f9e60b5a3249f9cbc0d92e171c7c8f71bc33d858
Leonardo Gonçalves <leomcg@gmail.com>
2020-06-07 12:54:16 -0300

    practicing pipes

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: string;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = value;
+  }
+}
